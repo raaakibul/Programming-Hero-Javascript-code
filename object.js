0477170c646273bfcda9a8ee1649eee7a67bf7b3
rakibul-islam <raakibul.uap@gmail.com>
2022-01-30 20:18:51 +0600

    object code

---
+++ object.js
@@ -0,0 +1,41 @@
+// student details
+// var names =  "Rakibul Islam"
+// var age = 23
+
+// var student1 ={
+//     names : "Rakibul",
+//     age : 23,
+// }
+
+// console.log(student1.age)
+
+// function getReminder(number1, number2) {
+//     number1 % number2;
+//   }
+  
+//   const reminder = getReminder(12, 2);
+  
+//   console.log(reminder);
+
+// function addNumber(number1, number2) {
+//     return "Result is " + number1 + number2;
+//    }
+   
+// var result = addNumber(5, 4);
+// console.log(result)
+
+// function isOdd(number){
+//     if(number%2 !=0){
+//         console.log("Odd")
+//     }
+//     else{
+//         console.log("Even")
+//     }
+// }
+// isOdd(45)
+ 
+var fact = 1
+for (var i=1; i<7;i++){
+    fact = fact *i;
+}
+console.log(fact)
